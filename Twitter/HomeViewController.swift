5edb9f3526c2771bc9d6fc63f67aa1ed9f359fac
Pratyaksh99 <31634863+Pratyaksh99@users.noreply.github.com>
2019-01-30 00:07:46 -0500

    Commit

--- Twitter/HomeViewController.swift
+++ Twitter/HomeViewController.swift
@@ -7,8 +7,45 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class HomeViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var num_tweets: Int!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        loadTweets()
+        
+    }
+    
+    func loadTweets() {
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            
+            for tweet in tweets {
+                
+                self.tweetArray.append(tweet)
+                
+            }
+            
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            
+            print("Error retrieving tweets!!!")
+            
+        })
+        
+    }
 
     
     @IBAction func logoutButton(_ sender: Any) {
@@ -21,26 +58,35 @@ class HomeViewController: UITableViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userName.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imgUrl = URL(string: (user["profile_image_url_https"] as? String)!)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        cell.profileImage.af_setImage(withURL: imgUrl!)
+        
+        return cell
+    
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return tweetArray.count
+        
     }
 
     /*
---
+++ Twitter/tweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  tweetCell.swift
+//  Twitter
+//
+//  Created by Pratyaksh Motwani on 1/29/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
