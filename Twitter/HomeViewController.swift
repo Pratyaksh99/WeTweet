4f0501ed8a352423459816e107d3e55a2a256d59
Pratyaksh99 <31634863+Pratyaksh99@users.noreply.github.com>
2019-02-05 20:42:10 -0500

    New Commit

--- Twitter/HomeViewController.swift
+++ Twitter/HomeViewController.swift
@@ -14,11 +14,16 @@ class HomeViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var num_tweets: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
         
         self.tableView.rowHeight = UITableView.automaticDimension
         self.tableView.estimatedRowHeight = 150
@@ -32,10 +37,10 @@ class HomeViewController: UITableViewController {
         
     }
     
-    func loadTweets() {
+    @objc func loadTweets() {
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": 20]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             
@@ -48,6 +53,7 @@ class HomeViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             
